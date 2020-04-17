ef62f12a2cafebba9c8e8bd012d984391d5079dd
benaor <benjamingirard25@gmail.com>
2020-04-17 23:16:05 +0200

    create function new article

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
@@ -19,5 +21,39 @@ class AdminController extends AbstractController
             'controller_name' => 'Espace Administrateur',
             'articles' => $articles
         ]);
+    }
+
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function new(EntityManagerInterface $manager, Request $request)
+    {
+        //Create new article
+        $article = new Article();
+
+        //recover data of the form
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $manager->persist($article);
+            $manager->flush();
+            return $this->redirectToRoute('admin');
+        }
+
+        return $this->render('admin/new.html.twig', [
+            'controller_name' => 'crée un nouvel article',
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/admin/{id}/delete", name="admin_delete")
+     */
+    public function delete(Article $article, EntityManagerInterface $manager)
+    {
+        $manager->remove($article);
+        $manager->flush();
+        return $this->redirectToRoute('admin');
     }
 }
